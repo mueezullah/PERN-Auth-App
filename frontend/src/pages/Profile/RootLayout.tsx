3c6fdc34f359b1f5b2444cf9c7249aa5cab9cc4f
mueezullah <mueezullahip8@gmail.com>
2026-06-27 00:01:48 +0500

    refresh profile sidebar

--- frontend/src/pages/Profile/RootLayout.tsx
+++ frontend/src/pages/Profile/RootLayout.tsx
@@ -7,7 +7,7 @@ export function RootLayout() {
   return (
     <div className="h-screen bg-slate-50 flex flex-col font-sans text-slate-900 overflow-hidden">
       <Navbar />
-      <main className="flex-1 mt-16 max-w-[1536px] mx-10 w-full grid grid-cols-1 lg:grid-cols-[220px_1fr] gap-0 lg:gap-1 px-0 lg:px-0">
+      <main className="flex-1 mt-16 max-w-384 mx-10 w-full grid grid-cols-1 lg:grid-cols-[220px_1fr] gap-0 lg:gap-1 px-0 lg:px-0">
         <div className="hidden lg:block h-[calc(100vh-64px)] overflow-y-auto relative [&::-webkit-scrollbar]:w-2 [&::-webkit-scrollbar-thumb]:bg-transparent hover:[&::-webkit-scrollbar-thumb]:bg-slate-300 [&::-webkit-scrollbar-thumb]:rounded-full transition-colors">
           <Sidebar />
         </div>
