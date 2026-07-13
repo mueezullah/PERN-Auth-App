ccef04b88c030fb78237ce50495e1119e98e59bc
mueezullah <mueezullahip8@gmail.com>
2026-07-13 23:07:04 +0500

    get specific post by id

--- backend/src/modules/posts/post.routes.js
+++ backend/src/modules/posts/post.routes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createPost, getAllPosts, getUserPosts, deletePost, updatePost } from "./post.controller.js";
+import { createPost, getAllPosts, getUserPosts, deletePost, updatePost, getPostById } from "./post.controller.js";
 import { ensureAuthenticated } from "../auth/auth.middleware.js";
 
 const router = express.Router();
@@ -7,6 +7,9 @@ const router = express.Router();
 // Routes for Posts
 // GET main feed (either public or logged in? usually authenticated users or public. Let's make it public to fetch, but require auth for some)
 router.get("/", getAllPosts);
+
+// GET specific post by ID
+router.get("/:id", getPostById);
 
 // GET specific user's posts
 router.get("/user/:userId", getUserPosts);
