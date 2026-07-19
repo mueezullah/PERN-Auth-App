2ea289be5d723a12163a9d13f5a076464a1041b4
mueezullah <mueezullahip8@gmail.com>
2026-07-19 23:05:50 +0500

    feat: add permanent comment deletion endpoint

--- backend/src/modules/comments/comment.routes.js
+++ backend/src/modules/comments/comment.routes.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { getComments, createComment } from "./comment.controller.js";
+import { getComments, createComment, deleteComment } from "./comment.controller.js";
 import { ensureAuthenticated } from "../auth/auth.middleware.js";
 
 const router = express.Router();
 
 router.get("/", getComments);
 router.post("/", ensureAuthenticated, createComment);
+router.delete("/:id", ensureAuthenticated, deleteComment);
 
 export default router;
