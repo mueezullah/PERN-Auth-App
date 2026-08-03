301ad69d7e804351e9eed2e4fdd81f998165954f
mueezullah <mueezullahip8@gmail.com>
2026-08-03 23:51:41 +0500

    add follows main routes

--- backend/src/dbInit.js
+++ backend/src/dbInit.js
@@ -4,6 +4,7 @@ import * as PostModel from "./modules/posts/post.model.js";
 import { initTable as initDonationTable } from "./modules/payments/donation.model.js";
 import * as CommentModel from "./modules/comments/comment.model.js";
 import * as LikeModel from "./modules/likes/like.model.js";
+import * as followModel from "./modules/follows/follow.model.js"
 
 export const initializeDatabaseSchema = async () => {
     try {
@@ -14,6 +15,7 @@ export const initializeDatabaseSchema = async () => {
         await initDonationTable();
         await CommentModel.initTable();
         await LikeModel.initTable();
+        await followModel.initTable();
         console.log("🚀 Database schema completely verified and initialized");
     } catch (err) {
         console.error("❌ Critical: Failed to initialize database tables:", err);
