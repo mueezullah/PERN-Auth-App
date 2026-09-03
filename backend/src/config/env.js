c536cd58e2c590908c2d63c57af4eafd68279ec6
mueezullah <mueezullahip8@gmail.com>
2026-09-03 23:34:20 +0500

    feat: export config variables

--- backend/src/config/env.js
+++ backend/src/config/env.js
@@ -18,6 +18,16 @@ const DB_PORT = process.env.DB_PORT;
 const DB_PASSWORD = process.env.DB_PASSWORD;
 const DB_NAME = process.env.DB_NAME || "pern_auth";
 
+const CLIENT_URL = process.env.CLIENT_URL || "http://localhost:5173";
+
+const STRIPE_SECRET_KEY = process.env.STRIPE_SECRET_KEY;
+const STRIPE_WEBHOOK_SECRET = process.env.STRIPE_WEBHOOK_SECRET;
+
+const AWS_ACCESS_KEY_ID = process.env.AWS_ACCESS_KEY_ID;
+const AWS_SECRET_ACCESS_KEY = process.env.AWS_SECRET_ACCESS_KEY;
+const AWS_REGION = process.env.AWS_REGION || "us-east-1";
+const AWS_S3_BUCKET_NAME = process.env.AWS_S3_BUCKET_NAME;
+
 export default {
   PORT,
   JWT_SECRET,
@@ -26,4 +36,11 @@ export default {
   DB_PORT,
   DB_PASSWORD,
   DB_NAME,
+  CLIENT_URL,
+  STRIPE_SECRET_KEY,
+  STRIPE_WEBHOOK_SECRET,
+  AWS_ACCESS_KEY_ID,
+  AWS_SECRET_ACCESS_KEY,
+  AWS_REGION,
+  AWS_S3_BUCKET_NAME,
 };
